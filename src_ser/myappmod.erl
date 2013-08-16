ab97dd97fe4d0fcf0aa6b15621513f1cc437ca97
Gokul <chandu.gokul138@gmail.com>
2013-08-16 02:22:29 +0200

    weired behaivour with yaws.conf

--- src_ser/myappmod.erl
+++ src_ser/myappmod.erl
@@ -5,12 +5,11 @@
 
 -include("/usr/local/lib/yaws/include/yaws_api.hrl").
 
+box(Str) ->
+    {'div',[{class,"box"}],
+     {pre,[],Str}}.
+
+
 out(A) ->
         {ehtml,
-     [{p,[],
-       box(io_lib:format("A#arg.appmoddata = ~p~n"
-                         "A#arg.appmod_prepath = ~p~n"
-                         "A#arg.querydata = ~p~n",
-                         [A#arg.appmoddata,
-                          A#arg.appmod_prepath,
-                          A#arg.querydata]))}]}.
+     [{p,[],"Hello"}]}.
