a375b15000ef22637dd57d8667ba78bdaeafc85c
Gokul <chandu.gokul138@gmail.com>
2013-08-20 00:16:20 +0200

    item key in url handeled and contact added in json objects

--- src_ser/post.erl
+++ src_ser/post.erl
@@ -19,10 +19,11 @@ out(A)->
     AppModData = A#arg.appmoddata,
     PrePath = A#arg.appmod_prepath,
     QueryData = A#arg.querydata,
-    {Latitude,Longitude} = get_position(QueryData),
+    {CrudePosition,Item} = get_item(QueryData), 
+    {Latitude,Longitude} = get_position(CrudePosition),
     case is_float(Latitude) of
 	true ->
-	    io:format("~n~p~n ~p~n ~p~n",[Latitude,Longitude,QueryData]);
+	    io:format("~nItem=~p~n ~p~n  ~p~n ~p~n",[Item,Latitude,Longitude,QueryData]);
 	false-> 
 	    io:format("Oh crap"),
 	    io:format("~p~n ~p~n ~p~n",[Latitude,Longitude,QueryData])
@@ -30,6 +31,18 @@ out(A)->
     {html, json2:encode(construct_JSON())}.
 
 
+construct_JSON()->
+    
+    Address1 = [{<<"StreetAddress">>, <<"21 2nd Street">>}, {<<"City">>, <<"New York">>},{<<"State">>, <<"NY">>},{<<"PostalCode">>, <<"10021">> },{"Contact","+46738928328"}],
+    Item1 = {struct, [{"ItemName","Estella Potato Chips"},{"price", "23.89"},{<<"Distance">>, <<"6.00">>},{<<"StoreName">>, <<"23.89">>}, {<<"Address">>, {struct, Address1}} ] },
+    Address2 = [{<<"StreetAddress">>, <<"21 2nd Street">>}, {<<"City">>, <<"New York">>},{<<"State">>, <<"NY">>},{<<"PostalCode">>, <<"10021">> },{"Contact","+46738928379"}],
+    Item2 = {struct, [{"ItemName", "OLW Chips"},{<<"Price">>, <<"23.89">>},{<<"Distance">>, <<"6.00">>},{<<"StoreName">>, <<"23.89">>}, {<<"Address">>,{struct, Address2}} ]},
+     
+    {array,[Item1,Item2]}.
+
+
+%% get_item()->
+%%     ok.
 
 get_position(QueryData)->
     {Tail, Latitude} = get_latitude(QueryData),
@@ -55,11 +68,11 @@ get_longitude([],List)->
 get_longitude([Num|Tail],List) ->
     get_longitude(Tail,[Num|List]).
 
-construct_JSON()->
-    
-    Address1 = [{<<"StreetAddress">>, <<"21 2nd Street">>}, {<<"City">>, <<"New York">>},{<<"State">>, <<"NY">>},{<<"PostalCode">>, <<"10021">> }],
-    Item1 = {struct, [{"ItemName","Estella Potato Chips"},{"price", "23.89"},{<<"Distance">>, <<"6.00">>},{<<"StoreName">>, <<"23.89">>}, {<<"Address">>, {struct, Address1}} ] },
-    Address2 = [{<<"StreetAddress">>, <<"21 2nd Street">>}, {<<"City">>, <<"New York">>},{<<"State">>, <<"NY">>},{<<"PostalCode">>, <<"10021">> }],
-    Item2 = {struct, [{"ItemName", "OLW Chips"},{<<"Price">>, <<"23.89">>},{<<"Distance">>, <<"6.00">>},{<<"StoreName">>, <<"23.89">>}, {<<"Address">>,{struct, Address2}} ]},
-     
-    {array,[Item1,Item2]}.
+
+get_item([105,116,101,109,61|Tail])->
+    get_item(Tail,[]).
+
+get_item([38|Tail],List)->
+    {Tail,lists:reverse(List)};
+get_item([Head|Tail],List) ->
+    get_item(Tail,[Head|List]).
